d43316be23a7768ea7ead2e44cde976c13a66f7e
drossoft <drossoft@outlook.com>
2023-08-11 18:49:18 +0200

    Buttons as ContentView causes Grid elements not clickable

---
+++ grid_issue_demo/CustomControls/Button1.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace grid_issue_demo.CustomControls;
+
+public partial class Button1 : ContentView
+{
+	public Button1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ grid_issue_demo/CustomControls/Button2.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace grid_issue_demo.CustomControls;
+
+public partial class Button2 : ContentView
+{
+	public Button2()
+	{
+		InitializeComponent();
+	}
+}
